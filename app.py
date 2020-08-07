3a9cf48abd9e6a7a10bbf4b178bea856ce92d97e
chaosannals <chaosannals@outlook.com>
2020-08-07 10:29:32 +0800

    ws

--- app.py
+++ app.py
@@ -4,8 +4,17 @@ from sanic.response import json
 app = Sanic()
 
 @app.route('/')
-async def test(request):
+async def index(request):
     return json({'hello': 'world'})
+
+@app.websocket('/api')
+async def work(request, ws):
+    while True:
+        data = 'hello!'
+        print('Sending: ' + data)
+        await ws.send(data)
+        data = await ws.recv()
+        print('Received: ' + data)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
